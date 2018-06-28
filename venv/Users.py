1cc5487ba8f60e12d11b19f433b4cd1143661ecb
TamerlaN0210 <timurahmadulin@mail.ru>
2018-06-28 16:59:32 +0500

    Сделал авторизацию и получение пользователя по _id

--- venv/Users.py
+++ venv/Users.py
@@ -2,26 +2,37 @@ import pymongo
 import copy
 from sanic import Sanic
 from sanic.response import json, text
-from json import dumps as jdumps
-from json import  loads as jloads
+from bson.objectid import ObjectId
+from passlib.hash import pbkdf2_sha256 as sha256
+
 
 client = pymongo.MongoClient()
 db = client.test
 users = db.users
 
 
-def user_validation(data_dictionary):
+def validate_user(data_dictionary):
     # TODO сделать проверку на значения
     if len(data_dictionary) == 3:
         if "username" in data_dictionary and "password" in data_dictionary and "created_at" in data_dictionary:
             return True
-    return False
+    else:
+        return False
+
+
+def validate_auth(data_dictionary):
+    # TODO сделать проверку на значения
+    if len(data_dictionary) == 2:
+        if "username" in data_dictionary and "password" in data_dictionary:
+            return True
+    else:
+        return False
 
 
 def does_user_exist(username):
     username = str(username)
-    querry_result = users.find_one({"username": username})
-    if querry_result is None:
+    querry = users.find_one({"username": username})
+    if querry is None:
         return False
     else:
         return True
@@ -32,13 +43,14 @@ app = Sanic(__name__)
 
 @app.route("/user/registry/", methods=["POST"])
 async def registry(request):
-    if user_validation(request.json):
+    if validate_user(request.json):
         username = request.json.get("username")
         if not does_user_exist(username):
+
             # копирую объект, т.к. при выполнении запроса к бд меняется request.json и сервер не работает
-            query_result = db.users.insert_one(copy.copy(request.json))
-            if query_result:
-                return json({"Verification": user_validation(request.json), "json_queue": request.json}, status=201)
+            querry = db.users.insert_one(copy.copy(request.json))
+            if querry:
+                return text(None, status=201)
             else:
                 return text("Error while writing in data base")
         else:
@@ -47,15 +59,26 @@ async def registry(request):
         return text("JSON must have only this keys: username, password, created_at")
 
 
-@app.route("/user/<name>", methods=['POST'])
-async def test(request, name):
-    # data = users.find_one({"name": "{}".format(name)})
-    return text('POST request - {}'.format(request.json))
+@app.route("/user/auth/", methods=["POST"])
+async def authorization(request):
+    if validate_auth(copy.copy(request.json)):
+        querry = users.find_one(request.json)
+        if querry is not None:
+            return json({"id": str(querry.get("_id"))})
+        else:
+            return text(None, status=401)
+    else:
+        return text("JSON must only have this keys: username, password.", status=401)
 
 
-@app.route("/user/<name>", methods=['GET'])
-async def test(request, name):
-    data = users.find_one({"name": "{}".format(request.json)})
-    return text(str(data))
+@app.route("/user/<user_id>", methods=["GET"])
+async def get_user(request, user_id):
+    querry = users.find_one({"_id": ObjectId(user_id)}, {"password": 0})
+    if querry is not None:
+        querry["_id"] = str(querry["_id"])
+        return json(querry)
+    else:
+        return text(None, status=401)
+
 
 app.run(host="0.0.0.0", port=8000)
